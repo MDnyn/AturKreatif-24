b91184109d5180949793d39762df78fe2594c472
Muhammad 'Adnin bin Ali Huddin <162657733+MDnyn@users.noreply.github.com>
2024-04-26 11:10:58 +0800

    Add files via upload

---
+++ WEB/Medium/2. head scratch/src/blacklist-guest.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>JANGAN DATANG KENDURI AKU</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+<h1> 1. NAQIB</h1>
+<h1> 2. NAQIB</h1>
+<h1> 3. NAQIB</h1>
+</body>
+</html>
