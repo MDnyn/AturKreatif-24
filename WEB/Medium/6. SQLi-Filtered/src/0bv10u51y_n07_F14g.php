b8bb919ba89b43b410a9c4c09caea349f36a4844
A_MyZain <122007749+AmmZain@users.noreply.github.com>
2024-05-09 09:38:42 -0400

    Add files via upload

---
+++ WEB/Medium/6. SQLi-Filtered/src/0bv10u51y_n07_F14g.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="img/auch.png">
+    <title>Flag</title>
+    <style>
+        body {
+            background-image: url('img/giphy.gif');
+            font-family: Arial, sans-serif;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        
+        .container {
+            max-width: 600px;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            text-align: center;
+        }
+        
+        h2 {
+            color: #333;
+        }
+        
+        p {
+            color: #666;
+            font-size: 18px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h2>Congratulations!</h2>
+        <p>You have successfully completed the challenge. Here is the flag: 4turkr34tif24{H4141_1NJ3c710N_!??}</p>
+    </div>
+</body>
+</html>
