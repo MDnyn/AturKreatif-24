9799690ea82f26b8841f47340af275acb581c2e2
haiqall01 <149815819+haiqall01@users.noreply.github.com>
2024-05-09 11:18:06 -0400

    Add files via upload

---
+++ CRYPTOGRAPHY/MEDIUM/AESy Image Riddle/release_file/Image-Cryptor.py
@@ -0,0 +1,25 @@
+def encrypt_file(input_file, output_file, password):
+    print("File encrypted successfully.")
+
+def decrypt_file(input_file, output_file, password):
+    print("Sorryy..Not to easy dear...this tool was modified by somebody..but he give this clue for u.. The image was encrypted using AES encryption with CBC mode and PKCS7 padding. A randomly generated IV (Initialization Vector) and salt were used for encryption. The encryption key was derived from the user-provided password using PBKDF2 with 1,000,000 iterations and a key length of 32 bytes.")
+
+def main():
+    option = input("Choose an option (encrypt/decrypt): ").lower()
+
+    if option == 'encrypt':
+        input_file = input("Enter the path of the file to encrypt: ")
+        output_file = input("Enter the desired name for the encrypted file: ")
+        password = input("Enter the password: ")
+        encrypt_file(input_file, output_file, password)
+    elif option == 'decrypt':
+        input_file = input("Enter the path of the encrypted file: ")
+        output_file = input("Enter the desired name for the decrypted file: ")
+        password = input("Enter the password: ")
+        decrypt_file(input_file, output_file, password)
+    else:
+        print("Invalid option. Please choose 'encrypt' or 'decrypt'.")
+
+if __name__ == "__main__":
+    main()
+
