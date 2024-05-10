28f43ca4e2ab340b4f4c7f763f028bde0830a8d2
haiqall01 <149815819+haiqall01@users.noreply.github.com>
2024-05-10 05:55:41 -0400

    Delete CRYPTOGRAPHY/EASY/Mirror Mayhem directory

--- CRYPTOGRAPHY/EASY/Mirror Mayhem/solve.py
+++
@@ -1,9 +0,0 @@
-from cryptography.fernet import Fernet
-
-key = ' gyPhfCRdWBbKNAZ8EAzDO1yzC4-weiCTzxRz4udb8oA='
-
-f = Fernet(key)
-
-token = 'gAAAAABmNFO9UlRAr3leKWFZUK2oyjhPjqcCiTnSjVqK7YOlkd-H90PSiswWYEwJQqP_vd9447oQ58Kv8uajtXSPBpjPUpAYd50Qa7YVv8hwSHg7_oRmjX4='
-
-print(f.decrypt(token))
