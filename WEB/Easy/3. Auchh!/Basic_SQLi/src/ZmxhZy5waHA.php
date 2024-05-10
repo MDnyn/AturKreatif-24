208c9f2f5f8515267f22d34fd1755060e0334ab6
A_MyZain <122007749+AmmZain@users.noreply.github.com>
2024-05-09 21:47:41 -0400

    Add files via upload

---
+++ WEB/Easy/3. Auchh!/Basic_SQLi/src/ZmxhZy5waHA.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flag</title>
+    <link rel="icon" type="image/x-icon" href="img/hack.png">
+    <style>
+        body {
+            background-image: url('img/code.png');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+            font-family: Arial, sans-serif;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        
+        .container {
+            max-width: 600px;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            text-align: center;
+        }
+        
+        h2 {
+            color: #333;
+        }
+        
+        p {
+            color: #666;
+            font-size: 18px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h2>Congratulations!</h2>
+        <p>You have successfully completed the challenge. Here is the flag: 4turkr34tif24{F1R5T_5t3P_PR0_SQLi}</p>
+    </div>
+</body>
+</html>
